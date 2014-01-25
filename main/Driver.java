b237f9819f5ca6cd9b98b396d3071e548168d584
tylergiallanza <tylergiallanza@gmail.com>
2014-01-25 12:06:10 -0700

    Basic NPC Implementation
    - NPCs have Gravity
    - BUG: NPCs rotate when the swinging animation is called
    - UNRELATED BUG: The lag is back, but on my Windows computer...

--- main/Driver.java
+++ main/Driver.java
@@ -12,8 +12,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 import core.*;
+import npc.*;
+import npc.Character;
 
 public class Driver {
 
@@ -23,24 +24,26 @@ public class Driver {
 	 *       - Water?
 	 *       - Add world-specific player files
 	 *       - Add new tools (weapon?)
-	 *       - Add enemies  
+	 *       - Add enemies
 	 *       - Update swing animation
 	 *       - Fix lag
-	 *       - Add Don Quixote mode 
+	 *       - Add Don Quixote mode
 	 *       - Add multiplayer
 	 *       - Expand ore generation
 	 */
 
 	/**
 	 * @BUGFIXES
-	 *       - Fix GUI image stretch error
-	 *       - Fix treegen over caves
-	 *       - Spawning should be on surface
+	 *           - Fix GUI image stretch error
+	 *           - Fix treegen over caves
+	 *           - Spawning should be on surface
+	 *     		 - The lag is back- and this time on Windows...
 	 */
 
 	private static double x;
 	private static double y;
 	private static Player player;
+	private static Character character;
 	public static String gameState = "start";
 	public static final int WORLDWIDTH = 3;
 	public static int WIDTH = 1200;
@@ -75,8 +78,10 @@ public class Driver {
 	public static void initGL() {
 
 		try {
-			if(fullscreen) Display.setFullscreen(fullscreen);
-			else Display.setDisplayMode(new DisplayMode(1200,800));
+			if (fullscreen)
+				Display.setFullscreen(fullscreen);
+			else
+				Display.setDisplayMode(new DisplayMode(1200,800));
 			Display.create();
 			WIDTH = Display.getWidth();
 			HEIGHT = Display.getHeight();
@@ -109,26 +114,28 @@ public class Driver {
 					map = new World();
 					map.load("NewMap0");
 					player = Player.fromFile("player");
-					changeX(-player.getX() + WIDTH/2);
+					changeX(-player.getX()+WIDTH/2);
 					break;
 				case "load":
 					gameState = "loadMaps";
 					break;
 				case "new":
 					System.out.println("new");
-					player = new Player(WIDTH/2, HEIGHT/2, "PNG", "person3.png");
-					map = new World("NewMap" + getMaps().length);
+					player = new Player(WIDTH/2,HEIGHT/2,"PNG","person3.png");
+					character = new Character(WIDTH/3,HEIGHT+500,"PNG",
+							"person1.png");
+					map = new World("NewMap"+getMaps().length);
 					gameState = "inGame";
 					break;
 				}
 			}
-		} else if(gameState.equals("loadMaps")){
+		} else if (gameState.equals("loadMaps")) {
 			Handler.drawMenu("Load");
 			Handler.update();
-			if(Handler.getChoice() != null){
-				for(String s : getMaps()){
-					System.out.println(Handler.getChoice().getText() + "," + s);
-					if(Handler.getChoice().getText().equals(s)){
+			if (Handler.getChoice()!=null) {
+				for (String s : getMaps()) {
+					System.out.println(Handler.getChoice().getText()+","+s);
+					if (Handler.getChoice().getText().equals(s)) {
 						map = new World();
 						map.load(s);
 						player = Player.fromFile("player");
@@ -143,7 +150,9 @@ public class Driver {
 			drawWorld(map);
 			drawInventory(player.getInventory());
 			player.update();
+			character.update();
 			drawPlayer(player);
+			drawCharacter(character);
 			updateActiveBlocks();
 		}
 		Display.sync(60);
@@ -156,14 +165,24 @@ public class Driver {
 		System.exit(0);
 	}
 
+	private static boolean drawCharacter(Character obj) {
+		try {
+			obj.getTexture().bind();
+			rect(obj.getX(),obj.getY(),obj.getWidth(),obj.getHeight(),1);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
 	private static boolean drawPlayer(Player obj) {
 		try {
 			obj.getTexture().bind();
 			if (obj.getDirection())
-				rect(obj.getX(),obj.getY(),obj.getWidth(),obj.getHeight(), 1);
+				rect(obj.getX(),obj.getY(),obj.getWidth(),obj.getHeight(),1);
 			else
 				rect(obj.getX()+obj.getWidth(),obj.getY(),-obj.getWidth(),
-						obj.getHeight(), 1);
+						obj.getHeight(),1);
 			if (((Player) obj).getSelectedSlot() instanceof Tool)
 				draw(((Tool) ((Player) obj).getSelectedSlot()),
 						obj.getDirection());
@@ -179,8 +198,15 @@ public class Driver {
 	private static boolean draw(GameObject obj) {
 		try {
 			obj.getTexture().bind();
-			if(obj instanceof Block) rect(obj.getX(),obj.getY(),obj.getWidth(),obj.getHeight(), (double)((Block) obj).breakTime/(Block.linkTime(((Block) obj).getType())));
-			else rect(obj.getX(),obj.getY(),obj.getWidth(),obj.getHeight(), 1);
+			if (obj instanceof Block)
+				rect(obj.getX(),
+						obj.getY(),
+						obj.getWidth(),
+						obj.getHeight(),
+						(double) ((Block) obj).breakTime
+								/(Block.linkTime(((Block) obj).getType())));
+			else
+				rect(obj.getX(),obj.getY(),obj.getWidth(),obj.getHeight(),1);
 			return true;
 		} catch (Exception e) {
 			return false;
@@ -216,10 +242,12 @@ public class Driver {
 		return map;
 	}
 
-	private static void rect(int x, int y, int width, int height, double transparency) {
+	private static void rect(int x, int y, int width, int height,
+			double transparency) {
 		// draw the actual rectangle
-		if(transparency < .15) transparency = .15;
-		GL11.glColor4f(1f, 1f, 1f, (float)transparency);
+		if (transparency<.15)
+			transparency = .15;
+		GL11.glColor4f(1f,1f,1f,(float) transparency);
 		GL11.glBegin(GL11.GL_QUADS);
 		GL11.glTexCoord2f(0,1);
 		GL11.glVertex2f(x,y);
@@ -235,10 +263,10 @@ public class Driver {
 	private static void tiltedrect(int x, int y, int width, int height,
 			int rotation) {
 		// draw the actual rectangle
-		GL11.glTranslatef(x + width/2, y, 0.0f);
-		GL11.glRotatef(rotation, 0.0f, 0.0f, 1.0f);
-        GL11.glTranslatef(-x - width/2, -y, 0.0f);
-		GL11.glColor4f(1f, 1f, 1f, 1f);
+		GL11.glTranslatef(x+width/2,y,0.0f);
+		GL11.glRotatef(rotation,0.0f,0.0f,1.0f);
+		GL11.glTranslatef(-x-width/2,-y,0.0f);
+		GL11.glColor4f(1f,1f,1f,1f);
 		GL11.glBegin(GL11.GL_QUADS);
 		GL11.glTexCoord2f(0,1);
 		GL11.glVertex2f(x+width/2,y);
@@ -289,39 +317,47 @@ public class Driver {
 	public static Player getPlayer() {
 		return player;
 	}
-	
-	public static void addBlock(Block b){
+
+	public static void addBlock(Block b) {
 		active.add(b);
 	}
-	
-	public static void updateActiveBlocks(){
-		if(active.size() == 0) return;
-		for(int i = active.size() - 1; i >= 0; i--){
-			try{
-			if(active.get(i).breakTime < Block.linkTime(active.get(i).getType())){
-				if(!active.get(i).equals(player.getBlockAtMouse()) || !Mouse.isButtonDown(0))	active.get(i).breakTime+=2;
-			}else 
+
+	public static void updateActiveBlocks() {
+		if (active.size()==0)
+			return;
+		for (int i = active.size()-1; i>=0; i--) {
+			try {
+				if (active.get(i).breakTime<Block.linkTime(active.get(i)
+						.getType())) {
+					if (!active.get(i).equals(player.getBlockAtMouse())
+							||!Mouse.isButtonDown(0))
+						active.get(i).breakTime += 2;
+				} else
+					active.remove(i);
+			} catch (Exception e) {
 				active.remove(i);
-			} catch(Exception e){active.remove(i);}
+			}
 		}
 	}
-	
-	public static void drawInventory(Inventory inv){
-		int spot = (WIDTH/2) - ((INVENTORY_ICON_WIDTH+5) * 5);
-		for(int i = 0; i < 10; i++){
-			try{
-			Item item = inv.getData()[i];
-			new Tile(item, 0, 0).getTexture().bind();
-			rect(spot - (int)x, HEIGHT - INVENTORY_ICON_WIDTH - 10 - (int)y, INVENTORY_ICON_WIDTH, INVENTORY_ICON_WIDTH, 1);
-			} catch (Exception e){}
-			spot += INVENTORY_ICON_WIDTH + 5;
+
+	public static void drawInventory(Inventory inv) {
+		int spot = (WIDTH/2)-((INVENTORY_ICON_WIDTH+5)*5);
+		for (int i = 0; i<10; i++) {
+			try {
+				Item item = inv.getData()[i];
+				new Tile(item,0,0).getTexture().bind();
+				rect(spot-(int) x,HEIGHT-INVENTORY_ICON_WIDTH-10-(int) y,
+						INVENTORY_ICON_WIDTH,INVENTORY_ICON_WIDTH,1);
+			} catch (Exception e) {
+			}
+			spot += INVENTORY_ICON_WIDTH+5;
 		}
 	}
-	
-	public static String[] getMaps(){
+
+	public static String[] getMaps() {
 		File[] files = new File("maps").listFiles();
 		String[] s = new String[files.length];
-		for(int i = 0; i < files.length; i++)
+		for (int i = 0; i<files.length; i++)
 			s[i] = files[i].getName();
 		return s;
 	}
--- npc/Character.java
+++ npc/Character.java
@@ -1,16 +1,38 @@
 package npc;
+
 import physics.*;
 import core.*;
 import main.Driver;
 
-public class Character extends GameObject implements Gravity{
+public class Character extends GameObject implements Gravity {
+
+	public static final double GRAVITY = .4;
+	public static double yVel;
 
 	public Character() {
-		// TODO Auto-generated constructor stub
+
 	}
-	
-	public void updateGravity(){
-		
+
+	public Character(int x, int y, String type, String name) {
+		super(x,y,type,name);
+		width = 36;
+		height = 72;
+		yVel = 0;
+	}
+
+	public void update() {
+		if (!Driver.getWorld().isPlayerOnMap(this)) {
+			updateGravity();
+		} else
+			yVel = 0;
+
+	}
+
+	public void updateGravity() {
+		if (yVel<=0) {
+			yPos += yVel;
+		}
+		yVel -= GRAVITY;
 	}
 
 }
